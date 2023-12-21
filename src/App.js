c93b70ce97ca23add924f88a151df99fb308c26e
Bryan F. Rocha <bryanrocha@Bryans-Air.attlocal.net>
2023-12-21 13:18:46 -0600

    web-deploy-fix-3

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 
 import './App.scss';
-import {HashRouter as Router,Route} from 'react-router-dom';
+import {HashRouter as Routes,Route, Routes} from 'react-router-dom';
 import Layout from './components/Layout'
 import Home from './components/Home';
 import About from './components/About';
@@ -10,14 +10,14 @@ import Skills from './components/Skills';
 function App() {
   return (
     <>
-    <Router basename={process.env.PUBLIC_URL}>
+    <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Home/>} />
         <Route path='about' element={<About/>} />
         <Route path='skills' element={<Skills/>} />
         <Route path='contact' element={<Contact/>} />
       </Route>
-    </Router>
+    </Routes>
     </>
     
   );
