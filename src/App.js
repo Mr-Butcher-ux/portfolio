572ae046c137cde0041082bdc0eb563521d12fd3
Bryan F. Rocha <bryanrocha@Bryans-Air.attlocal.net>
2023-12-21 12:56:14 -0600

    web-deploy-fix-1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 
 import './App.scss';
-import {Route, Routes} from 'react-router-dom'
+import {BrowserRouter as Router,Route} from 'react-router-dom';
 import Layout from './components/Layout'
 import Home from './components/Home';
 import About from './components/About';
